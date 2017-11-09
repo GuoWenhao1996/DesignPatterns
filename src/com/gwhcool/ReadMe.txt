e58571b1799eb56b109dfd424dfa83b6397de87e
GuoWenhao1996 <1842297753@qq.com>
2017-11-09 15:53:54 +0800

    工厂方法模式和策略模式

--- src/com/gwhcool/ReadMe.txt
+++ src/com/gwhcool/ReadMe.txt
@@ -1,3 +1,3 @@
 包							名称			简介			时间
 com.gwhcool.simpleFactory	简单工厂模式	术业有专攻		2017-11-6 16:34:32
-com.gwhcool.factoryMethod	工厂方法模式
+com.gwhcool.factoryMethod	工厂方法模式	精益求精		2017-11-9 15:10:32
--- src/com/gwhcool/factoryMethod/Main.java
+++ src/com/gwhcool/factoryMethod/Main.java
@@ -1,5 +1,21 @@
 package com.gwhcool.factoryMethod;
 
 public class Main {
+	public static void main(String[] args) {
+		AnimalFactory factory = new TigerFactory();
+		Animal animal = factory.createAnimal();
+		animal.eat();
+		((Tiger) animal).run();
 
+		factory = new DelphinFactory();
+		animal = factory.createAnimal();
+		animal.eat();
+		((Delphin) animal).swim();
+
+		// Exception in thread "main" java.lang.ClassCastException:
+		// com.gwhcool.factoryMethod.Delphin cannot be cast to
+		// com.gwhcool.factoryMethod.Tiger
+		// at com.gwhcool.factoryMethod.Main.main(Main.java:15)
+		((Tiger) animal).run();
+	}
 }
